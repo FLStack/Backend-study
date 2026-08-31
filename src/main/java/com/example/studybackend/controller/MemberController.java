ba69e8b00f0e9010f0495f51120eb2d522e1a0ce
leebin9999 <ksi9999bin@naver.com>
2026-08-31 17:27:26 +0900

    fix: DB 저장/삭제 커밋을 위한 @Transactional 추가 및 포트 변경

--- src/main/java/com/example/studybackend/controller/MemberController.java
+++ src/main/java/com/example/studybackend/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.example.studybackend.domain.Member;
 import com.example.studybackend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class MemberController {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
     @PostMapping
     public Member createMember(@RequestBody Member member) {
         return memberRepository.save(member);
@@ -26,7 +28,7 @@ public class MemberController {
         return memberRepository.findAll();
     }
 
-    // Member 삭제 API 추가
+    @Transactional // DB 삭제
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMember(@PathVariable Long id) {
         memberRepository.deleteById(id);
